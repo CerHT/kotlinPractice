621954a798ef597918a258f2a8a06822b90d4b34
chenhantao <hantaochen@alog.com>
2019-09-20 18:12:40 +0800

    待续

--- src/cht/tree/rbtree/RBTree.kt
+++ src/cht/tree/rbtree/RBTree.kt
@@ -39,11 +39,14 @@ class RBTree<T : Comparable<T>> {
         // 这里使用了kotlin的 ?. 判断符，表达式左侧任意为null，就跳过
         b?.left?.parent = a
 
+        // 左旋后，a的父节点变为b的父节点
         b?.parent = a.parent
 
+        // a有可能是根节点，因为左旋后，b可能会变成根节点
         if (a.parent == null) {
             this.root = b
         } else {
+            // 变更后，需要更改父节点的子节点信息，即 pa的左右子节点
             if (a.parent?.left == a) {
                 a.parent?.left = b
             } else {
@@ -51,7 +54,9 @@ class RBTree<T : Comparable<T>> {
             }
         }
 
+        // a变为b的左节点
         b?.left = a
+        // b变为a的父节点
         a.parent = b
     }
 
@@ -70,14 +75,18 @@ class RBTree<T : Comparable<T>> {
      * @param b 右旋的节点
      */
     private fun rightRotate(b: RBTNode<T>) {
+        // 获取b的左节点a
         val a = b.left
 
+        // b的左节点变为a的右节点
         b.left = a?.right
-
+        // ra不为空的话，父节点变为b
         a?.right?.parent = b
 
+        // 右旋后，b的父节点变为a的父节点
         a?.parent = b.parent
 
+        // b有可能是根节点
         if (b.parent == null) {
             this.root = a
         } else {
@@ -93,6 +102,58 @@ class RBTree<T : Comparable<T>> {
 
     }
 
+    private fun insert(node: RBTNode<T>) {
+        var cmp: Int
+        var temp: RBTNode<T>? = null
+        var sign: RBTNode<T>? = this.root
+
+        // 1. 从根节点找起，将红黑树当做二叉查找树，将节点添加到二叉查找树中
+        while (sign != null) {
+            temp = sign
+            cmp = sign.key?.let { node.key?.compareTo(it) } ?: 0
+            // 如果node < root，则取更小的来比较
+            sign = if (cmp < 0) {
+                sign.right
+            } else {
+                sign.left
+            }
+        }
+        // 对比过后，找到node的位置,将node的设置在某个节点下
+        node.parent = temp
+
+        // 把node设置为别的节点的子节点
+        if (temp != null) {
+            cmp = temp.key?.let { node.key?.compareTo(it) } ?: 0
+            if (cmp < 0) {
+                temp.left = node
+            } else {
+                temp.right = node
+            }
+        } else {
+            this.root = node
+        }
+        // 2. 设置节点为红色
+        node.color = RED
+
+        // 3. 修正为二叉查找树
+        insertFix(node)
+    }
+
+    private fun insertFix(rbtNode: RBTNode<T>) {
+        var node = rbtNode
+
+        var gParent: RBTNode<T>?
+
+        // 若父节点存在，且父节点为红色
+        var parent: RBTNode<T>? = parentOf(node)
+        while (parent != null && isRed(parent)) {
+            gParent = parentOf(parent)
+            if (parent == gParent)
+
+        }
+
+
+    }
 
     // 一些简单的方法，从Java版参考，有些可能不需要
     private fun minNode(node: RBTNode<T>?): RBTNode<T>? {

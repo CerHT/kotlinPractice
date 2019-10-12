739487d985e954e36c60449f8fcd152f10680453
chenhantao <hantaochen@alog.com>
2019-10-12 14:47:44 +0800

    update

--- src/cht/queen/Queen.kt
+++ src/cht/queen/Queen.kt
@@ -27,11 +27,32 @@ class Queen(private val max_size: Int) {
         return true
     }
 
-    fun settleQueen(y: Int): Boolean {
+    fun init() {
+        settleQueen(0)
+    }
+
+    private fun settleQueen(y: Int): Boolean {
         // 行数超过最大，说明已经找到答案
         if (y == max_size) {
             return true
         }
         // 遍历当前行，逐一格子验证
+        for (i in 0 until max_size) {
+            // 为当前行清零，以免在回溯的时候出现脏数据
+            for (x in 0 until max_size) {
+                chessBoard[x][y] = 0
+            }
+            // 检查是否符合规则，如果符合，更改元素值并进一步递归
+            if (check(i, y)) {
+                chessBoard[i][y] = 1
+                // 递归如果返回true，说明下层已找到解决办法，无需继续循环
+                if (settleQueen(y + 1)) {
+                    return true
+                }
+            }
+        }
+        return false
     }
+
+
 }
